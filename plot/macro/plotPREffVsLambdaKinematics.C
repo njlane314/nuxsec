28abaf63c86050a2df30f151a9bac5ca947861c2
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-10 20:52:51 +0000

    Shorten efficiency axis title in PR plots

--- plot/macro/plotPREffVsLambdaKinematics.C
+++ plot/macro/plotPREffVsLambdaKinematics.C
@@ -233,7 +233,7 @@ int plotPREffVsLambdaKinematics(const std::string &samples_tsv = "",
         EfficiencyPlot::Config cfg;
         cfg.x_title = v.x_title;
         cfg.y_counts_title = "Events";
-        cfg.y_eff_title = "Pattern recognition efficiency";
+        cfg.y_eff_title = "Efficiency";
         cfg.legend_total = "Denominator";
         cfg.legend_passed = "Numerator";
         cfg.legend_eff = "Efficiency";
