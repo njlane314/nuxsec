34271892fc4a75305d85bb7a645d66ff308cf834
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-10 15:17:26 +0000

    Change canvas width from 800 to 600 pixels

--- plot/macro/plotLegendOnlyNoStats.C
+++ plot/macro/plotLegendOnlyNoStats.C
@@ -51,7 +51,7 @@ void plotLegendOnlyNoStats(const char *output_name = "legend_only_no_stats.pdf")
 {
     gStyle->SetOptStat(0);
 
-    TCanvas canvas("c_legend_only", "Legend only", 800, 125);
+    TCanvas canvas("c_legend_only", "Legend only", 600, 125);
     canvas.SetFillColor(kWhite);
     canvas.SetFrameFillColor(kWhite);
 
