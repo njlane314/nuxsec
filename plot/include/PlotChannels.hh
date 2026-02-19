a67339ab6d29a3adf4edb20767f51df15ea794d4
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-19 19:57:20 +0000

    Update plot channel palette to MicroBooNE-style scheme

--- plot/include/PlotChannels.hh
+++ plot/include/PlotChannels.hh
@@ -70,20 +70,30 @@ class Channels
     {
         static const std::map<int, Properties> m = {
             {0, {0, "data", "Data", kBlack, 1001}},
-            {1, {1, "external", "Cosmic", colour_from_hex("#1f2a44"), 3345}},
-            {2, {2, "out_fv", "Out FV", colour_from_hex("#ff9f1c"), 1001}},
-            {10, {10, "numu_cc_np0pi", "#nu_{#mu}CC Np0#pi", colour_from_hex("#3a86ff"), 1001}},
-            {11, {11, "numu_cc_0pnpi", "#nu_{#mu}CC 0p1#pi^{#pm}", colour_from_hex("#8338ec"), 1001}},
-            {12, {12, "numu_cc_pi0gg", "#nu_{#mu}CC #pi^{0}/#gamma#gamma", colour_from_hex("#ff006e"), 1001}},
-            {13, {13, "numu_cc_npnpi", "#nu_{#mu}CC multi-#pi^{#pm}", colour_from_hex("#5e60ce"), 1001}},
-            {14, {14, "nc", "#nu_{x}NC", colour_from_hex("#4361ee"), 1001}},
-            {15, {15, "signal_lambda_ccqe", "Signal #Lambda^{0} CCQE (#Lambda^{0} #rightarrow p#pi^{-})", colour_from_hex("#39ff14"), 1001}},
-            {16, {16, "signal_lambda_ccres", "Signal #Lambda^{0} CCRES (#Lambda^{0} #rightarrow p#pi^{-})", colour_from_hex("#00e676"), 1001}},
+            //
+            // Palette aligned to the (MicroBooNE-style) scheme in the reference figure:
+            //   - signals: greens
+            //   - #nu_{#mu} CC: monochrome blues (light #rightarrow dark for subchannels)
+            //   - NC: yellow
+            //   - #nu_{e} CC: magenta
+            //   - out-FV: brown
+            //   - cosmic: neutral grey (hatched)
+            //   - other: dark red
+            //
+            {1, {1, "external", "Cosmic", colour_from_hex("#bababa"), 3345}},
+            {2, {2, "out_fv", "Out FV", colour_from_hex("#80603e"), 1001}},
+            {10, {10, "numu_cc_np0pi", "#nu_{#mu}CC Np0#pi", colour_from_hex("#2f5cf9"), 1001}},
+            {11, {11, "numu_cc_0pnpi", "#nu_{#mu}CC 0p1#pi^{#pm}", colour_from_hex("#2347e0"), 1001}},
+            {12, {12, "numu_cc_pi0gg", "#nu_{#mu}CC #pi^{0}/#gamma#gamma", colour_from_hex("#1832c7"), 1001}},
+            {13, {13, "numu_cc_npnpi", "#nu_{#mu}CC multi-#pi^{#pm}", colour_from_hex("#0c1dae"), 1001}},
+            {14, {14, "nc", "#nu_{x}NC", colour_from_hex("#fbcf38"), 1001}},
+            {15, {15, "signal_lambda_ccqe", "Signal #Lambda^{0} CCQE (#Lambda^{0} #rightarrow p#pi^{-})", colour_from_hex("#5cfd3f"), 1001}},
+            {16, {16, "signal_lambda_ccres", "Signal #Lambda^{0} CCRES (#Lambda^{0} #rightarrow p#pi^{-})", colour_from_hex("#48ca31"), 1001}},
             {17, {17, "signal_lambda_ccdis", "Signal #Lambda^{0} CCDIS (#Lambda^{0} #rightarrow p#pi^{-})", colour_from_hex("#7ae582"), 1001}},
             {18, {18, "signal_lambda_ccother", "Signal #Lambda^{0} CC Other (#Lambda^{0} #rightarrow p#pi^{-})", colour_from_hex("#2dc653"), 1001}},
-            {19, {19, "nue_cc", "#nu_{e}CC", colour_from_hex("#00f5d4"), 1001}},
-            {20, {20, "numu_cc_other", "#nu_{#mu}CC Other", colour_from_hex("#118ab2"), 1001}},
-            {99, {99, "other", "Other", colour_from_hex("#ff3d00"), 1001}}};
+            {19, {19, "nue_cc", "#nu_{e}CC", colour_from_hex("#c110f9"), 1001}},
+            {20, {20, "numu_cc_other", "#nu_{#mu}CC Other", colour_from_hex("#000895"), 1001}},
+            {99, {99, "other", "Other", colour_from_hex("#c32910"), 1001}}};
         return m;
     }
 };
