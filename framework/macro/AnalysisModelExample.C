bd24793fdc78d4120d9e5d429715163302d4ac34
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-21 18:28:29 +0000

    Wrap AnalysisModelExample macro body in try/catch

--- framework/macro/AnalysisModelExample.C
+++ framework/macro/AnalysisModelExample.C
@@ -17,24 +17,31 @@
 
 void AnalysisModelExample(const char *sample_list_path = "scratch/out/out/sample/samples.tsv")
 {
-    nu::ExecutionPolicy policy{.nThreads = 4, .enableImplicitMT = nu::ExecutionPolicy::env_enabled("HERON_PLOT_IMT"), .deterministic = true};
-    policy.apply("AnalysisModelExample");
-
-    heron::Dataset dataset;
-    if (!dataset.load_samples(sample_list_path))
+    try
     {
-        throw std::runtime_error(std::string("AnalysisModelExample: failed to load sample list: ") + sample_list_path);
+        nu::ExecutionPolicy policy{.nThreads = 4, .enableImplicitMT = nu::ExecutionPolicy::env_enabled("HERON_PLOT_IMT"), .deterministic = true};
+        policy.apply("AnalysisModelExample");
+
+        heron::Dataset dataset;
+        if (!dataset.load_samples(sample_list_path))
+        {
+            throw std::runtime_error(std::string("AnalysisModelExample: failed to load sample list: ") + sample_list_path);
+        }
+
+        heron::AnalysisContext<nu::ExecutionPolicy> context(policy, "default");
+
+        DefaultAnalysisModel &model = const_cast<DefaultAnalysisModel &>(DefaultAnalysisModel::instance());
+        model.initialise(context);
+
+        std::cout << "[AnalysisModelExample] model=" << model.name()
+                  << " tree=" << model.tree_name()
+                  << " selections=" << model.selections().size()
+                  << " loaded_samples=" << dataset.samples().size()
+                  << " from=" << sample_list_path
+                  << "\n";
     }
-
-    heron::AnalysisContext<nu::ExecutionPolicy> context(policy, "default");
-
-    DefaultAnalysisModel &model = const_cast<DefaultAnalysisModel &>(DefaultAnalysisModel::instance());
-    model.initialise(context);
-
-    std::cout << "[AnalysisModelExample] model=" << model.name()
-              << " tree=" << model.tree_name()
-              << " selections=" << model.selections().size()
-              << " loaded_samples=" << dataset.samples().size()
-              << " from=" << sample_list_path
-              << "\n";
+    catch (const std::exception &e)
+    {
+        std::cerr << "[AnalysisModelExample] " << e.what() << "\n";
+    }
 }
