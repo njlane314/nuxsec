b59cbc7199a11c8b06bab2704b778ebfeba4f002
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-21 23:29:39 +0000

    Rename TemplateBinningOptimiser1D source to .cc

--- framework/modules/ana/src/TemplateBinningOptimiser1D.cc
+++ framework/modules/ana/src/TemplateBinningOptimiser1D.cc
@@ -1,6 +1,6 @@
 /* -- C++ -- */
 /**
- *  @file  framework/ana/src/TemplateBinningOptimizer1D.cc
+ *  @file  framework/ana/src/TemplateBinningOptimiser1D.cc
  *
  *  @brief Implementation for template-based 1D binning optimisation.
  */
